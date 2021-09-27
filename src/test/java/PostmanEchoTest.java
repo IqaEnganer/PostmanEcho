7f5208ff968e4e6e204775cce19b6878949def69
iqaenganer <daw.camalian@yandex.ru>
2021-09-28 00:36:09 +0300

    Тесты чтобы сборка упала

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,21 +1,26 @@
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 
 public class PostmanEchoTest {
     @Test
-    void shouldReturnDemoAccounts() {
+    void shouldShowDataPost() {
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .contentType("text/plain; charset=UTF-8")
+                .body("Проверочные данные")
+                // отправляемые данные (заголовки и query можно выставлять аналогично)
                 // Выполняемые действия
                 .when()
                 .post("/post")
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("",hasSize(2))
+                .body("headers.content-length", equalTo("35"))
+                .body("data", equalTo("Проверочные данны"))
+
         ;
     }
 }
